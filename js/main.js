e62afd6b2c9d32be2223879cf44f9508ca193f3f
Ashley <ashley.ratigan@gmail.com>
2018-05-26 10:23:54 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 // create an array to store all cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamondss",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 // create an array to store cards in play
 var cardsInPlay = [];
@@ -16,10 +37,12 @@ var checkForMatch = function(){
 // function to store all steps when card is flipped
 var flipCard = function(cardId){
 	// display card user just flipped
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].suit);
 
 	//add card to array of cards in play
-	cardsInPlay.push(cards[cardId]);	
+	cardsInPlay.push(cards[cardId].rank);	
 
 	// check to see if two cards played
 	if (cardsInPlay.length === 2) {
